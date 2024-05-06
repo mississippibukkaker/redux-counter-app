e31a71e4a8a8a3984eec2a42a4aa8526c5f9c496
mississippibukkaker <mktds13@outlook.jp>
2024-05-06 11:50:50 +0900

    count取得

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import  counterReducer  from "./counterSlice";
 
 // 状態を一元管理している場所、store
 export const store = configureStore({
-    reducer: {},
-})
+  reducer: { counter: counterReducer },
+});
