76b2bb8b5c49a92ac580bc3189b86759a4b4e5f7
mississippibukkaker <mktds13@outlook.jp>
2024-05-06 13:44:56 +0900

    入力フォーム, 追加ボタンの実装

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { increment } from "./redux/counterSlice";
-import { decrement } from "./redux/counterSlice";
+import { increment, decrement, incrementByAmount } from "./redux/counterSlice";
+import { useState } from "react";
 
 function App() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+
+  const [incrementAmount, setIncrementAmount] = useState("2");
   return (
     <div className="App">
       <h1>count : {count}</h1>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
+      <input
+        onChange={(e) => setIncrementAmount(e.target.value)}
+        value={incrementAmount}
+      />
+      <button onClick={() => dispatch(increment())}>＋</button>
+      <button onClick={() => dispatch(decrement())}>ー</button>
+      <button
+        onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}
+      >
+        追加
+      </button>
     </div>
   );
 }
